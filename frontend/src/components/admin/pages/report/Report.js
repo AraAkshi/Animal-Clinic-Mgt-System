293871413295c0a85565a93a89d8992b0d930e32
Ishara Ishak <ishakishara7@gmail.com>
2021-04-21 05:45:01 +0530

    Merge with working tree

--- frontend/src/components/admin/pages/report/Report.js
+++ frontend/src/components/admin/pages/report/Report.js
@@ -1,12 +1,12 @@
 import React, { useState, useRef } from 'react';
 import ReactToPrint from 'react-to-print';
 import {
-	Grid,
-	Button,
-	CircularProgress,
-	TextField,
-	MenuItem,
-	Select,
+  Grid,
+  Button,
+  CircularProgress,
+  TextField,
+  MenuItem,
+  Select,
 } from '@material-ui/core';
 import Header from '../../layout/Header';
 import Sidebar from '../../layout/Sidebar';
@@ -17,106 +17,106 @@ import { formatDate } from '../../../../services/appointment';
 import { reportTypes } from '../../../../services/datasets/report-types.d';
 
 function Report() {
-	const d = new Date();
-	const date = formatDate(d);
-	const [StartDate, setStartDate] = useState(date);
-	const [EndDate, setEndDate] = useState(date);
-	const [reportType, setReportType] = useState('');
-	const [loading, setLoading] = useState(null);
+  const d = new Date();
+  const date = formatDate(d);
+  const [StartDate, setStartDate] = useState(date);
+  const [EndDate, setEndDate] = useState(date);
+  const [reportType, setReportType] = useState('');
+  const [loading, setLoading] = useState(null);
 
-	const onStartDateSelect = (e) => {
-		setStartDate(e.target.value);
-	};
+  const onStartDateSelect = (e) => {
+    setStartDate(e.target.value);
+  };
 
-	const onEndDateSelect = (e) => {
-		setEndDate(e.target.value);
-	};
+  const onEndDateSelect = (e) => {
+    setEndDate(e.target.value);
+  };
 
-	const getReportData = async () => {
-		setLoading(false);
-		const res = await getSalesByDate(StartDate, EndDate);
-		setLoading(true);
-	};
+  const getReportData = async () => {
+    setLoading(false);
+    const res = await getSalesByDate(StartDate, EndDate);
+    setLoading(true);
+  };
 
-	return (
-		<div>
-			<Header />
-			<Sidebar />
-			<div className='sidebar-container'>
-				<Grid
-					container
-					direction='row'
-					justify='center'
-					spacing={3}
-					style={{ marginTop: '0.5rem' }}
-				>
-					<Grid item>
-						<TextField
-							label='Start Date'
-							variant='outlined'
-							type='date'
-							size='small'
-							name='startDate'
-							value={StartDate}
-							style={{ backgroundColor: '#fff' }}
-							onChange={(e) => onStartDateSelect(e)}
-						/>
-					</Grid>
-					<Grid item>
-						<TextField
-							label='End Date'
-							variant='outlined'
-							type='date'
-							size='small'
-							name='EndDate'
-							value={EndDate}
-							style={{ backgroundColor: '#fff' }}
-							onChange={(e) => onEndDateSelect(e)}
-						/>
-					</Grid>
-					<Grid item xs={3}>
-						<Select
-							// labelId='petName'
-							name='type'
-							value={reportType}
-							// onChange={(e) => onChange(e)}
-							required
-						>
-							{reportTypes.length > 0 ? (
-								reportTypes.map((item) => (
-									<MenuItem key={item.id} value={item.type}>
-										{item.type}
-									</MenuItem>
-								))
-							) : (
-								<MenuItem>No Report Types</MenuItem>
-							)}
-						</Select>
-						{/* <Select options={options} label='Report Type' /> */}
-					</Grid>
-					<Grid item>
-						<Button
-							size='small'
-							variant='contained'
-							color='secondary'
-							onClick={getReportData}
-							endIcon={
-								loading === false ? (
-									<CircularProgress size='1rem' />
-								) : loading === true ? (
-									<CheckCircleOutlineOutlinedIcon />
-								) : (
-									true
-								)
-							}
-						>
-							Get Report
-						</Button>
-					</Grid>
-				</Grid>
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <Header />
+      <Sidebar />
+      <div className="sidebar-container">
+        <Grid
+          container
+          direction="row"
+          justify="center"
+          spacing={3}
+          style={{ marginTop: '0.5rem' }}
+        >
+          <Grid item>
+            <TextField
+              label="Start Date"
+              variant="outlined"
+              type="date"
+              size="small"
+              name="startDate"
+              value={StartDate}
+              style={{ backgroundColor: '#fff' }}
+              onChange={(e) => onStartDateSelect(e)}
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              label="End Date"
+              variant="outlined"
+              type="date"
+              size="small"
+              name="EndDate"
+              value={EndDate}
+              style={{ backgroundColor: '#fff' }}
+              onChange={(e) => onEndDateSelect(e)}
+            />
+          </Grid>
+          <Grid item xs={3}>
+            <Select
+              // labelId='petName'
+              name="type"
+              value={reportType}
+              // onChange={(e) => onChange(e)}
+              required
+            >
+              {reportTypes.length > 0 ? (
+                reportTypes.map((item) => (
+                  <MenuItem key={item.id} value={item.type}>
+                    {item.type}
+                  </MenuItem>
+                ))
+              ) : (
+                <MenuItem>No Report Types</MenuItem>
+              )}
+            </Select>
+            {/* <Select options={options} label='Report Type' /> */}
+          </Grid>
+          <Grid item>
+            <Button
+              size="small"
+              variant="contained"
+              color="secondary"
+              onClick={getReportData}
+              endIcon={
+                loading === false ? (
+                  <CircularProgress size="1rem" />
+                ) : loading === true ? (
+                  <CheckCircleOutlineOutlinedIcon />
+                ) : (
+                  true
+                )
+              }
+            >
+              Get Report
+            </Button>
+          </Grid>
+        </Grid>
+      </div>
+    </div>
+  );
 }
 
 export default Report;
--- frontend/src/components/admin/pages/treatments/Treatment.js
+++ frontend/src/components/admin/pages/treatments/Treatment.js
@@ -1,21 +1,19 @@
 import {
-	TableContainer,
-	Paper,
-	Grid,
-	Table,
-	TableHead,
-	TableCell,
-	TableRow,
-	Typography,
-	Button,
-	TableBody,
-	TextField,
+  TableContainer,
+  Paper,
+  Grid,
+  Table,
+  TableHead,
+  TableCell,
+  TableRow,
+  Typography,
+  Button,
+  TableBody,
+  TextField,
 } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 import AddIcon from '@material-ui/icons/Add';
 import React, { useEffect, useState } from 'react';
-import { DatePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
-import MomentUtils from '@date-io/moment';
 import { getAllCustomers } from '../../../../services/customer';
 import { formatDate } from '../../../../services/appointment';
 import { getAllTreatments } from '../../../../services/treatment';
@@ -27,211 +25,211 @@ import { getCategoryItems } from '../../../../services/inventory';
 import { getCusAnimals } from '../../../../services/animal';
 
 const StyledTableCell = withStyles((theme) => ({
-	head: {
-		backgroundColor: theme.palette.common.black,
-		color: theme.palette.common.white,
-	},
-	body: {
-		fontSize: 13,
-	},
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 13,
+  },
 }))(TableCell);
 
 function Treatment() {
-	const d = new Date();
-	const date = formatDate(d);
-	const [alert, setAlert] = useState([]);
-	const [selectedDate, setSelectedDate] = useState(date);
-	const [treatments, setTreatments] = useState([
-		{
-			id: 0,
-			treatmentType: '',
-			customer: { name: '', id: '', contact: '' },
-			animal: { name: '', breed: '', id: '' },
-			itemsUsed: [],
-			description: '',
-			dateReceived: '',
-			timeReceived: '',
-			nextTreatmentDate: '',
-		},
-	]);
-	const [todayTreatments, setTodayTreatments] = useState([
-		{
-			id: 0,
-			treatmentType: '',
-			customer: { name: '', id: '', contact: '' },
-			animal: { name: '', breed: '', id: '' },
-			itemsUsed: [],
-			description: '',
-			dateReceived: '',
-			timeReceived: '',
-			nextTreatmentDate: '',
-		},
-	]);
-	const [items, setItems] = useState([]);
-	const [customers, setCustomers] = useState([]);
-	const [cusAnimals, setCusAnimals] = useState([]);
-	const [selectedTreatment, setSelectedTreatment] = useState({
-		id: 0,
-		treatmentType: '',
-		customer: { name: '', id: '', contact: '' },
-		animal: { name: '', breed: '', id: '' },
-		itemsUsed: [],
-		description: '',
-		dateReceived: '',
-		timeReceived: '',
-		nextTreatmentDate: '',
-	});
+  const d = new Date();
+  const date = formatDate(d);
+  const [alert, setAlert] = useState([]);
+  const [selectedDate, setSelectedDate] = useState(date);
+  const [treatments, setTreatments] = useState([
+    {
+      id: 0,
+      treatmentType: '',
+      customer: { name: '', id: '', contact: '' },
+      animal: { name: '', breed: '', id: '' },
+      itemsUsed: [],
+      description: '',
+      dateReceived: '',
+      timeReceived: '',
+      nextTreatmentDate: '',
+    },
+  ]);
+  const [todayTreatments, setTodayTreatments] = useState([
+    {
+      id: 0,
+      treatmentType: '',
+      customer: { name: '', id: '', contact: '' },
+      animal: { name: '', breed: '', id: '' },
+      itemsUsed: [],
+      description: '',
+      dateReceived: '',
+      timeReceived: '',
+      nextTreatmentDate: '',
+    },
+  ]);
+  const [items, setItems] = useState([]);
+  const [customers, setCustomers] = useState([]);
+  const [cusAnimals, setCusAnimals] = useState([]);
+  const [selectedTreatment, setSelectedTreatment] = useState({
+    id: 0,
+    treatmentType: '',
+    customer: { name: '', id: '', contact: '' },
+    animal: { name: '', breed: '', id: '' },
+    itemsUsed: [],
+    description: '',
+    dateReceived: '',
+    timeReceived: '',
+    nextTreatmentDate: '',
+  });
 
-	//Get treatments of selected date
-	const onChange = (e) => {
-		setSelectedDate(e.target.value);
-		const todayTreatments = treatments.filter((item) => {
-			if (formatDate(item.dateReceived) === e.target.value) return item;
-		});
-		setTodayTreatments(todayTreatments);
-	};
+  //Get treatments of selected date
+  const onChange = (e) => {
+    setSelectedDate(e.target.value);
+    const todayTreatments = treatments.filter((item) => {
+      if (formatDate(item.dateReceived) === e.target.value) return item;
+    });
+    setTodayTreatments(todayTreatments);
+  };
 
-	//Add Treatment
-	const addTreatment = () => {
-		window.open(
-			window.location.origin + '/admin/treatments/add-treatment',
-			'_self'
-		);
-	};
+  //Add Treatment
+  const addTreatment = () => {
+    window.open(
+      window.location.origin + '/admin/treatments/add-treatment',
+      '_self'
+    );
+  };
 
-	const handleRowSelect = async (item) => {
-		setSelectedTreatment(item);
+  const handleRowSelect = async (item) => {
+    setSelectedTreatment(item);
 
-		//Get Animals of the Customer
-		const animalRes = await getCusAnimals(item.customer.id);
-		if (animalRes !== undefined) setCusAnimals(animalRes);
-	};
+    //Get Animals of the Customer
+    const animalRes = await getCusAnimals(item.customer.id);
+    if (animalRes !== undefined) setCusAnimals(animalRes);
+  };
 
-	useEffect(() => {
-		async function fetchData() {
-			const res = await getAllTreatments();
-			if (res !== undefined) {
-				setTreatments(res);
-				const todayTreatments = res.filter(
-					(item) => item.dateReceived === selectedDate
-				);
-				setTodayTreatments(todayTreatments);
-			}
+  useEffect(() => {
+    async function fetchData() {
+      const res = await getAllTreatments();
+      if (res !== undefined) {
+        setTreatments(res);
+        const todayTreatments = res.filter(
+          (item) => item.dateReceived === selectedDate
+        );
+        setTodayTreatments(todayTreatments);
+      }
 
-			const customerRes = await getAllCustomers();
-			if (customerRes !== undefined) setCustomers(customerRes);
+      const customerRes = await getAllCustomers();
+      if (customerRes !== undefined) setCustomers(customerRes);
 
-			const itemsRes = await getCategoryItems(1);
-			if (itemsRes !== undefined) setItems(itemsRes);
-		}
-		fetchData();
-	}, [0]);
+      const itemsRes = await getCategoryItems(1);
+      if (itemsRes !== undefined) setItems(itemsRes);
+    }
+    fetchData();
+  }, [0]);
 
-	return (
-		<div>
-			<Alerts alerts={alert} />
-			<Header />
-			<Sidebar />
-			<div className='sidebar-container'>
-				<Grid container direction='row' justify='space-between'>
-					<Grid item>
-						<Grid container direction='row' alignContent='center'>
-							<Grid item>
-								<Typography variant='body1' style={{ paddingTop: '0.81rem' }}>
-									Total Treatments
-								</Typography>
-							</Grid>
-							<Grid item>
-								<div className='petStatCard'>{todayTreatments.length}</div>
-							</Grid>
-						</Grid>
-					</Grid>
-					<Grid item>
-						<Button
-							size='small'
-							color='secondary'
-							startIcon={<AddIcon />}
-							onClick={addTreatment}
-							variant='contained'
-							style={{ margin: '0.5rem' }}
-						>
-							New Treatment
-						</Button>
-					</Grid>
-				</Grid>
-				<hr className='seperatorLine' />
-				<Grid container direction='row' justify='space-between'>
-					<Grid item xs={6}>
-						<Grid container direction='row' justif='flex-start' spacing={2}>
-							<Grid item>
-								<Typography variant='subtitle2'>Select Date: </Typography>
-							</Grid>
-							<Grid item>
-								<TextField
-									type='date'
-									size='small'
-									name='selectedDate'
-									value={selectedDate}
-									onChange={(e) => onChange(e)}
-								/>
-							</Grid>
-						</Grid>
-						<TableContainer
-							component={Paper}
-							style={{
-								marginTop: '0.5rem',
-								maxHeight: '70vh',
-								overflowY: 'auto',
-							}}
-						>
-							<Table size='small' stickyHeader>
-								<TableHead>
-									<TableRow>
-										<StyledTableCell>Date</StyledTableCell>
-										<StyledTableCell>Customer</StyledTableCell>
-										<StyledTableCell>Animal</StyledTableCell>
-										<StyledTableCell>Treatment Type</StyledTableCell>
-									</TableRow>
-								</TableHead>
-								<TableBody>
-									{todayTreatments.length > 0 ? (
-										todayTreatments.map((item) => (
-											<TableRow
-												key={item.id}
-												hover={true}
-												onClick={() => handleRowSelect(item)}
-												style={{ cursor: 'pointer' }}
-											>
-												<StyledTableCell>
-													{formatDate(item.dateReceived)}
-												</StyledTableCell>
-												<StyledTableCell>{item.customer.name}</StyledTableCell>
-												<StyledTableCell>{item.animal.name}</StyledTableCell>
-												<StyledTableCell>{item.treatmentType}</StyledTableCell>
-											</TableRow>
-										))
-									) : (
-										<TableRow>
-											<StyledTableCell>No Treatments</StyledTableCell>
-										</TableRow>
-									)}
-								</TableBody>
-							</Table>
-						</TableContainer>
-					</Grid>
-					<Grid item xs={6}>
-						<TreatmentDetails
-							selectedTreatment={selectedTreatment}
-							setAlert={setAlert}
-							customers={customers}
-							items={items}
-							cusAnimals={cusAnimals}
-						/>
-					</Grid>
-				</Grid>
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <Alerts alerts={alert} />
+      <Header />
+      <Sidebar />
+      <div className="sidebar-container">
+        <Grid container direction="row" justify="space-between">
+          <Grid item>
+            <Grid container direction="row" alignContent="center">
+              <Grid item>
+                <Typography variant="body1" style={{ paddingTop: '0.81rem' }}>
+                  Total Treatments
+                </Typography>
+              </Grid>
+              <Grid item>
+                <div className="petStatCard">{todayTreatments.length}</div>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item>
+            <Button
+              size="small"
+              color="secondary"
+              startIcon={<AddIcon />}
+              onClick={addTreatment}
+              variant="contained"
+              style={{ margin: '0.5rem' }}
+            >
+              New Treatment
+            </Button>
+          </Grid>
+        </Grid>
+        <hr className="seperatorLine" />
+        <Grid container direction="row" justify="space-between">
+          <Grid item xs={6}>
+            <Grid container direction="row" justif="flex-start" spacing={2}>
+              <Grid item>
+                <Typography variant="subtitle2">Select Date: </Typography>
+              </Grid>
+              <Grid item>
+                <TextField
+                  type="date"
+                  size="small"
+                  name="selectedDate"
+                  value={selectedDate}
+                  onChange={(e) => onChange(e)}
+                />
+              </Grid>
+            </Grid>
+            <TableContainer
+              component={Paper}
+              style={{
+                marginTop: '0.5rem',
+                maxHeight: '70vh',
+                overflowY: 'auto',
+              }}
+            >
+              <Table size="small" stickyHeader>
+                <TableHead>
+                  <TableRow>
+                    <StyledTableCell>Date</StyledTableCell>
+                    <StyledTableCell>Customer</StyledTableCell>
+                    <StyledTableCell>Animal</StyledTableCell>
+                    <StyledTableCell>Treatment Type</StyledTableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {todayTreatments.length > 0 ? (
+                    todayTreatments.map((item) => (
+                      <TableRow
+                        key={item.id}
+                        hover={true}
+                        onClick={() => handleRowSelect(item)}
+                        style={{ cursor: 'pointer' }}
+                      >
+                        <StyledTableCell>
+                          {formatDate(item.dateReceived)}
+                        </StyledTableCell>
+                        <StyledTableCell>{item.customer.name}</StyledTableCell>
+                        <StyledTableCell>{item.animal.name}</StyledTableCell>
+                        <StyledTableCell>{item.treatmentType}</StyledTableCell>
+                      </TableRow>
+                    ))
+                  ) : (
+                    <TableRow>
+                      <StyledTableCell>No Treatments</StyledTableCell>
+                    </TableRow>
+                  )}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Grid>
+          <Grid item xs={6}>
+            <TreatmentDetails
+              selectedTreatment={selectedTreatment}
+              setAlert={setAlert}
+              customers={customers}
+              items={items}
+              cusAnimals={cusAnimals}
+            />
+          </Grid>
+        </Grid>
+      </div>
+    </div>
+  );
 }
 
 export default Treatment;
